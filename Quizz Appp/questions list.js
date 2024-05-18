b2ee818b9a2c6543d9af5621abcf370b636e08e8
MIHIRmm77 <142740505+MIHIRmm77@users.noreply.github.com>
2024-05-18 15:41:27 +0530

    Add files via upload

---
+++ Quizz Appp/questions list.js
@@ -0,0 +1,95 @@
+let questions = [
+  {
+    numb: 1,
+    question: " Which one of the following is the size of int arr[9] assuming that int is of 4 bytes?",
+    answer: "36",
+    options: [
+      "9",
+      "36",
+      "35",
+      "None"
+    ]
+  },
+  {
+    numb: 2,
+    question: " Which one of the following is the process of inserting an element in the stack?",
+    answer: "Push",
+    options: [
+      "Insert",
+      "Add",
+      "Push",
+      "None"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When the user tries to delete the element from the empty stack then the condition is said to be a",
+    answer: "Underflow",
+    options: [
+      "Underflow",
+      "Overflow",
+      "Garbage Collection",
+      "None of above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+ 
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
